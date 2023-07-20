623213ac2bd19a23d54e25425dfa26f951a0f616
Justin-mario <79143260+Justin-mario@users.noreply.github.com>
2023-07-20 23:54:38 +0100

    first commit

---
+++ src/test/kotlin/com/bank_app/MyBankAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bank_app
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MyBankAppApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
